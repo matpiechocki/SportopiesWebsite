0038b207a4c6cba54fe4e6943eca7077565ff385
Mateusz Piechocki <80588513+matpiechocki@users.noreply.github.com>
2022-03-27 19:34:59 +0200

    SportopiesWebsite

---
+++ WebsiteSportopies/html.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="ikona.png">
+	<meta name="author" content="Mateusz Piechocki">
+	<meta name="keywords" content="sport, zima, pies, husky">
+	<meta name="description" content="Strona przeznaczona dla osób pasjonujących się sportem z psami">
+	<title>Blog | Sportopies</title>
+</head>
+<body>
+<a name="top"></a>
+<header>
+	<h1>Sportopies</h1>
+	<h2>Blog <br> psich sportów</h2>
+</header>
+<div id="return">
+	<a href="#top">
+		<img src="top.png" alt="top">
+	</a>
+</div>
+<nav>
+	<ul>
+	<li>
+		<a href="index.php">SPORT</a>
+	</li>
+	<li>
+		<a href="sprzet.php">SPRZĘT</a>
+	</li>
+	<li>
+		<a href="zawody.php">ZAWODY</a>
+	</li>
+	</ul>
+</nav>
+<article>
+	<hr>
+	<h2 class="tytul">
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	Canicross-Jaki sprzęt wybrać? Jak przygotować siebie i psa?
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	</h2>
+	<p class="tresc">Przydadzą się umiejętności takie jak: ciągła chęć biegania, startowanie na odliczanie, skręcanie na komendy, szybkie biegniecie do przodu. Ważne jest aby zacząć treningi od małych dystansów np. 1km i zaczynać bieg ustaloną komendą wprowadzając psa tym samym w szybki bieg utrzymujemy jego tempo i nagradzamy napiętą smycz oraz poprawne skręcenie.
+	</p>
+	<p class="tresc">
+	Do canicrossu potrzebujemy wygodne szelki dla psa, w których pies będzie mógł ciągnąć bez pododuszania się. 
+	Dobrym wyborem są szelki zaprzęgowe typu sled lub sportowe szelki o przedłużonym tyle. Odradza się szelek w typie norweskim ponieważ blokuje on naturalny ruch łopatek. 
+	</p>
+	<img class="galery" src="o6.jpg" alt="obraz6" width="313" height="520">
+	<img class="galery" src="o7.jpg" alt="obraz7" width="313" height="520">
+	<p class="tresc"> Pas maszera pozwala nam zaczepić psa bezpośrednio do na, dzięki czemu mamy wolne ręce by podczas biegu pracować nimi.  Należy zwrócić szczególną uwagę na dobór pasa odpowiedniego dla nas. Psy wolniejsze, o mniejszej masie mogą być zaczepione do pasów szerszych, które nosimy wyżej na lędźwiach. Szybkie, silne i mocne psy będą powodowały zbyt duże obciążenia na nasz kręgosłup. Wówczas wybierzmy pas gdzie środek uciągu będzie niżej np;. na kościach kulszowych, tak by pies ciągną bardziej za biodra niż za krzyż. 
+	</p>
+	<img class="galery" src="o8.jpg" alt="obraz8" width="800" height="467">
+	<p class="tresc"> Ostatnim elementem jest smycz z amortyzatorem, lub amortyzator + smycz, które pozwalają zminimalizować szarpnięcia zarówno samego psa jak i te odczuwalne na naszym kręgosłupie.
+	</p>
+	<img class="galery" src="o9.jpg" alt="obraz9" width="650" height="290">
+	<hr>
+	<h2 class="tytul">
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	Bikejorng
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	</h2>
+	<p class="tresc">Rowery do bikejoringu to zazwyczaj rowery MTB (popularnie nazywane “górskimi”) hardtail (z angielskiego”sztywny ogon” czyli bez tylnego amortyzatora). Musi on być wyposażony w niezależne hamulce na przód i tył oraz posiadać wysięgnik do bikejoringu.
+	</p>
+	<p class="tresc"> Nauka komend typu “prawo”, “lewo” czy “stój” to absolutne podstawy do tego, by móc bezpiecznie i sprawnie pokonywać wyznaczone trasy.
+W dalszym etapie szkolenia pies powinien być również przyzwyczajany do bezproblemowego wyprzedzania innych teamów, z którymi będziemy konkurować na zawodach.
+Psiego sportowca do wysiłku należy przygotowywać stopniowo i najlepiej pod okiem doświadczonego zawodnika, który pomoże nam ustalić odpowiedni plan treningowy i da niezbędne wskazówki.
+	</p>
+	<p class="tresc">Najczęściej zawodnicy dla swoich czworonożnych sportowców, wybierają szelki typu sled. 
+	</p>
+	<img class="galery" src="o10.jpg" alt="obraz10" width="599" height="500">	
+	<hr>
+	<h2 class="tytul">
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	Dogtrekking 
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	</h2>
+	<p class="tresc">Warto na początek wprowadzić trekking raz w tygodniu, a z czasem rozszerzyć nawet do trzech razy w tygodniu.
+
+Stopniowo też zmieniają trasy trekkingu z psem. Zacznijcie od płaskich terenów. Zdecydowanie wybierajcie ścieżki bez asfaltu! Omijajcie także chodniki. Lepiej będzie spacerować po lesie czy łąkach
+    </p>
+    <p class="tresc">Dogtrekking to sport, który nie wymaga wielkich nakładów finansowych, specjalistycznego sprzętu czy wiedzy. Podstawą są odpowiednio dobrane szelki np typu sportowego-sled po guardy. Kolejnym istotnym elementem jest pas biodrowy
+	</p>
+	<img class="galery" src="o12.jpeg" alt="obraz12" width="749" height="500">
+	<hr>
+	<h2 class="tytul">
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	Psie zaprzęgi
+	<img class ="symbole" src="symbol.png" alt="symbol">
+	</h2>
+	<p class="tresc">Psie zaprzęgi dzielą się na wiele rodzi. Wiele zależy od wyboru wózka zaprzęgowego. Dzielą się one na hulanogi, dwukołowce oraz czterokołowce.
+	</p>
+	<p class="tresc">Odróżniamy też Skijoring czyli  maszer, będący przyczepiony do psa lub psów linką z amortyzatorem, porusza się na nartach biegowych
+    </p>
+	<p class="tresc">  Pulka – to dyscyplina, którą zapoczątkowali skandynawscy myśliwi i tam ona jest najbardziej popularna. Pulka to płaskie saneczki, które posiadają dwa dyszle, połączone pałąkiem, pomiędzy którymi przypięte są psy w ilości do jednego do czterech. Natomiast narciarz przypięty jest specjalną liną do pulki
+	</p>
+	<img class="galery" src="o13.jpg" alt="obraz13" width="576" height="383">
+    <p class="tresc"> 4 kołowa maszyna służy do pracy i wyścigów z reguły z zaprzęgiem 5-8 psów. 
+    </p>
+	<img class="galery" src="o14.jpg" alt="obraz14" width="500" height="666">
+	
+	
+	
+	
+	</article>
+<footer>
+	<hr>
+	<p>Mateusz Piechocki 2021 &copy </p>
+	<a href="https://www.youtube.com/" target="_blank">
+	<img id="youtube" class="ikony" src="youtube.png" alt="yt"  width="50" height="50">
+	</a>
+	<a href="https://www.instagram.com/" target="_blank">
+	<img id="instagram" class="ikony" src="instagram.png" alt="ig"  width="50" height="50">
+	</a>
+	<a href="https://www.facebook.com/" target="_blank">
+	<img id="facebook" class="ikony" src="facebook.png" alt="fb"  width="50" height="50">
+	</a>
+	<hr>
+</footer>
+</html> 
